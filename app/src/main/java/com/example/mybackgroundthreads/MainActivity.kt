f5d9f1060ce6a5674fa7b9009aa7b9af7a437fe5
Nuril Muslichin <nurilmuslichin16@gmail.com>
2020-10-14 15:53:08 +0700

    Corountine Kotlin

---
+++ app/src/main/java/com/example/mybackgroundthreads/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.mybackgroundthreads
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val INPUT_STRING = "Halo Ini Demo AsyncTask!!"
+        private const val LOG_ASYNC = "DemoAsync"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tv_status.setText(R.string.status_pre)
+        tv_desc.text = INPUT_STRING
+
+        GlobalScope.launch(Dispatchers.IO) {
+            //Background Threads
+            val input = INPUT_STRING
+
+            var output: String? = null
+
+            Log.d(LOG_ASYNC, "status : doInBackground")
+            try {
+                //Input stringnya ditambahkan dengan string 'selamat belajar!!'
+                output = "$input Selamat Belajar!!"
+
+                delay(2000)
+
+                //pindah ke Main Thread untuk update UI
+                withContext(Dispatchers.Main) {
+                    tv_status.setText(R.string.status_post)
+                    tv_desc.text = output
+                }
+            } catch (e: Exception) {
+                Log.d(LOG_ASYNC, e.message.toString())
+            }
+        }
+    }
+}
